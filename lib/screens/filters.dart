933e7e0c54f1924ad3fdfd0f16d0b76970b922e3
Tu_Dev <nguyenvantu.cnpm@gmail.com>
2024-04-30 21:49:16 +0700

    all

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -7,39 +7,116 @@ class FilterScreen extends StatefulWidget {
     return _FilterScreenState();
   }
 }
+enum Filter{
+  glutenFree,
+  lactoseFree,
+  vegeFree,
+  
+
+}
 
 class _FilterScreenState extends State<FilterScreen> {
   var _filterSet = false;
+  var _meatSet = false;
+  var _vegetableSet = false;
+
+  var _fishSet = false;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Filter King'),
+        title: Text('Filter'),
       ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: _filterSet,
-            onChanged: (isChecked){
-              _filterSet = isChecked;
-            },
-            title: Text(
-              'Setting ',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (didPop){
+          if(didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _filterSet,
+            Filter.lactoseFree: _meatSet,
+            Filter.vegeFree: _vegetableSet,
+          });
+        },
+        
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _filterSet,
+              onChanged: (isChecked){
+                setState(() {
+                _filterSet = isChecked;
+                  
+                });
+              },
+              title: Text(
+                'Filter a ',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Only a filter to this',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 30, right: 20),
             ),
-            subtitle: Text(
-              'Only king',
-              style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+        
+            SwitchListTile(
+              value: _meatSet,
+              onChanged: (isChecked){
+                setState((){
+                _meatSet = isChecked;
+        
+                });
+              },
+              title: Text(
+                'meat filter ',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Only meat filter this',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 30, right: 20),
             ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(left: 30, right: 20),
-          ),
-        ],
+        
+             SwitchListTile(
+              value: _vegetableSet,
+              onChanged: (isChecked){
+                setState(() {
+                _vegetableSet = isChecked;
+                  
+                });
+              },
+              title: Text(
+                'vegetable ',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Only vegetable this',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 30, right: 20),
+            ),
+        
+            
+          ],
+        ),
       ),
     );
   }
